a27b08d4a214ef427f584eace0c5d9732ba2fb75
Archie Gelonga <acgelonga@gmail.com>
2021-02-15 11:32:46 +0800

    Login page UI

--- assets/containers/Login/index.js
+++ assets/containers/Login/index.js
@@ -1,10 +1,74 @@
-import React from 'react';
-import { Button } from 'antd'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Form, Input, Checkbox, Button, Layout, Col, Row } from "antd";
 
 export default function Login() {
-    return (
-        <>
-            <Button type="primary">Login</Button>
-        </>
-    );
-  };
+  const [loading, setLoading] = useState(false);
+  const [form] = Form.useForm();
+  const handleSubmit = () => {};
+  return (
+    <>
+      <Layout>
+        <Row justify="center">
+          <Col span={6}>
+            <Form
+              form={form}
+              name="normal_login"
+              className="user-form"
+              initialValues={{ remember: true }}
+              onFinish={handleSubmit}
+              autoComplete="off"
+            >
+              <Form.Item
+                name="email"
+                rules={[
+                  { required: true, message: "Please enter your Email!" },
+                ]}
+              >
+                <Input size="large" type="text" placeholder="E-mail Address" />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[
+                  { required: true, message: "Please enter your Password!" },
+                ]}
+              >
+                <Input size="large" type="password" placeholder="Password" />
+              </Form.Item>
+              <Form.Item>
+                <Row justify="space-between">
+                  <Col>
+                    <Form.Item name="remember" valuePropName="checked" noStyle>
+                      <Checkbox>Keep me signed in</Checkbox>
+                    </Form.Item>
+                  </Col>
+                  <Col>
+                    <a className="user-form-forgot" href="/request-password">
+                      Forgot your password?
+                    </a>
+                  </Col>
+                </Row>
+              </Form.Item>
+
+              <Form.Item>
+                <Button
+                  block
+                  loading={loading}
+                  type="primary"
+                  htmlType="submit"
+                  className="user-form-button"
+                >
+                  Log in
+                </Button>
+                <p style={{ marginTop: "1rem", textAlign: "center" }}>
+                  Don't have an account?{` `}
+                  <Link to="/register">Sign Up</Link>
+                </p>
+              </Form.Item>
+            </Form>
+          </Col>
+        </Row>
+      </Layout>
+    </>
+  );
+}
