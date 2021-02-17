5048044e30ee657334945152503242e12e0fa3a3
jasummercity-mac <jandavearce@jasummercity-macs-MacBook-Pro.local>
2021-02-17 14:32:53 +0800

    Added code formatter

--- assets/components/Charts/MixedChart/index.js
+++ assets/components/Charts/MixedChart/index.js
@@ -1,86 +1,80 @@
-import React from "react";
-import Chart from "react-apexcharts";
+import React from 'react'
+import Chart from 'react-apexcharts'
 
 const series = [
+  {
+    name: 'Registrations',
+    type: 'column',
+    data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16],
+  },
+  {
+    name: 'Vistis',
+    type: 'line',
+    data: [440, 505, 414, 671, 227, 413, 201, 352, 752, 320, 257, 160],
+  },
+]
+
+const apexChart = {
+  series: [
     {
-      name: "Registrations",
-      type: "column",
+      name: 'Registrations',
+      type: 'column',
       data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16],
     },
     {
-      name: "Vistis",
-      type: "line",
+      name: 'Visits',
+      type: 'line',
       data: [440, 505, 414, 671, 227, 413, 201, 352, 752, 320, 257, 160],
     },
-  ];
-
-  const apexChart = {
-    series: [
-      {
-        name: "Registrations",
-        type: "column",
-        data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16],
+  ],
+  chart: {
+    height: 350,
+    type: 'line',
+  },
+  stroke: {
+    width: [0, 4],
+  },
+  title: {
+    text: 'Website Visits',
+  },
+  dataLabels: {
+    enabled: true,
+    enabledOnSeries: [1],
+  },
+  labels: [
+    '01 Jan 2001',
+    '02 Jan 2001',
+    '03 Jan 2001',
+    '04 Jan 2001',
+    '05 Jan 2001',
+    '06 Jan 2001',
+    '07 Jan 2001',
+    '08 Jan 2001',
+    '09 Jan 2001',
+    '10 Jan 2001',
+    '11 Jan 2001',
+    '12 Jan 2001',
+  ],
+  xaxis: {
+    type: 'datetime',
+  },
+  yaxis: [
+    {
+      title: {
+        text: 'Registrations',
       },
-      {
-        name: "Visits",
-        type: "line",
-        data: [440, 505, 414, 671, 227, 413, 201, 352, 752, 320, 257, 160],
+    },
+    {
+      opposite: true,
+      title: {
+        text: 'Visits',
       },
-    ],
-    chart: {
-      height: 350,
-      type: "line",
     },
-    stroke: {
-      width: [0, 4],
-    },
-    title: {
-      text: "Website Visits",
-    },
-    dataLabels: {
-      enabled: true,
-      enabledOnSeries: [1],
-    },
-    labels: [
-      "01 Jan 2001",
-      "02 Jan 2001",
-      "03 Jan 2001",
-      "04 Jan 2001",
-      "05 Jan 2001",
-      "06 Jan 2001",
-      "07 Jan 2001",
-      "08 Jan 2001",
-      "09 Jan 2001",
-      "10 Jan 2001",
-      "11 Jan 2001",
-      "12 Jan 2001",
-    ],
-    xaxis: {
-      type: "datetime",
-    },
-    yaxis: [
-      {
-        title: {
-          text: "Registrations",
-        },
-      },
-      {
-        opposite: true,
-        title: {
-          text: "Visits",
-        },
-      },
-    ],
-  };
-
-function MixedChart() {
-  return (
-    <Chart
-        options={apexChart}
-        series={series}
-        type="line"
-    />
-  );
+  ],
 }
 
-export default MixedChart;
+function MixedChart() {
+  return <Chart options={apexChart} series={series} type="line" />
+}
+
+export default MixedChart
--- assets/containers/Overview/index.js
+++ assets/containers/Overview/index.js
@@ -1,11 +1,11 @@
-import React, { useMemo } from 'react';
+import React, { useMemo } from 'react'
 
 export default function Overview() {
-    return useMemo(() => {
-        return (
-            <>
-                <h1>Overview Component</h1>
-            </>
-        );
-    },[])
-  };
+  return useMemo(() => {
+    return (
+      <>
+        <h1>Overview Component</h1>
+      </>
+    )
+  }, [])
+}
