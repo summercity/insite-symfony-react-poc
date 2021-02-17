a2e90a78185101af3649ee9c538fe41a284d48ca
Archie Gelonga <acgelonga@gmail.com>
2021-02-17 13:50:27 +0800

    Memoize

--- assets/components/DatePicker/index.js
+++ assets/components/DatePicker/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useMemo } from "react";
 import moment from "moment";
 import { Context } from "./../../context/ContextStore";
 import { SET_DATE_FILTER } from "./../../context/constant";
@@ -18,7 +18,8 @@ const ALL = "ALL";
 const CUSTOM = "CUSTOM";
 
 const DatePicker = () => {
-  const [state, dispatch] = React.useContext(Context);
+  const [state, dispatch] = useContext(Context);
+  const { dateFilter } = state;
   const [preset, setPreset] = React.useState(TODAY);
   const [isDatePickerVisible, setIsDatePickerVisible] = React.useState(false);
 
@@ -152,47 +153,50 @@ const DatePicker = () => {
     }
   };
 
-  return (
-    <>
-      <Select
-        style={{ width: 180 }}
-        defaultValue={TODAY}
-        value={preset}
-        onChange={handlePresetChange}
-      >
-        <Option key={1} value={TODAY}>
-          Today
-        </Option>
-        <Option key={2} value={LAST_7_DAYS}>
-          Past 7 Days
-        </Option>
-        <Option key={3} value={LAST_14_DAYS}>
-          Past 14 Days
-        </Option>
-        <Option key={4} value={LAST_30_DAYS}>
-          Past 30 Days
-        </Option>
-        <Option key={5} value={CURRENT_MONTH}>
-          Current Month
-        </Option>
-        <Option key={6} value={PREVIOUS_MONTH}>
-          Previous Month
-        </Option>
-        <Option key={7} value={PREVIOUS_QUARTER}>
-          Previous Quarter
-        </Option>
-        <Option key={8} value={ALL}>
-          All Recorded Data
-        </Option>
-        <Option key={9} value={CUSTOM}>
-          Custom Range
-        </Option>
-      </Select>
-      {!!isDatePickerVisible && (
-        <RangePicker style={{ marginLeft: 15 }} onChange={handleChange} />
-      )}
-    </>
-  );
+  return useMemo(() => {
+    return (
+      <>
+        {console.log("rerender")}
+        <Select
+          style={{ width: 180 }}
+          defaultValue={TODAY}
+          value={preset}
+          onChange={handlePresetChange}
+        >
+          <Option key={1} value={TODAY}>
+            Today
+          </Option>
+          <Option key={2} value={LAST_7_DAYS}>
+            Past 7 Days
+          </Option>
+          <Option key={3} value={LAST_14_DAYS}>
+            Past 14 Days
+          </Option>
+          <Option key={4} value={LAST_30_DAYS}>
+            Past 30 Days
+          </Option>
+          <Option key={5} value={CURRENT_MONTH}>
+            Current Month
+          </Option>
+          <Option key={6} value={PREVIOUS_MONTH}>
+            Previous Month
+          </Option>
+          <Option key={7} value={PREVIOUS_QUARTER}>
+            Previous Quarter
+          </Option>
+          <Option key={8} value={ALL}>
+            All Recorded Data
+          </Option>
+          <Option key={9} value={CUSTOM}>
+            Custom Range
+          </Option>
+        </Select>
+        {!!isDatePickerVisible && (
+          <RangePicker style={{ marginLeft: 15 }} onChange={handleChange} />
+        )}
+      </>
+    );
+  }, [dateFilter, dispatch]);
 };
 
 export default DatePicker;
