9eb042aaec0011c102b72f906b37d7ebb0071f87
jasummercity-mac <jandavearce@jasummercity-macs-MacBook-Pro.local>
2021-02-18 09:25:07 +0800

    Added LaodingIndicator for lazy loading or code splitting

---
+++ assets/components/LoadingIndicator/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './loader.scss'
+
+const LoadingIndicator = () => (
+  <div className="lds-ripple">
+    <div></div>
+    <div></div>
+  </div>
+)
+
+export default LoadingIndicator
