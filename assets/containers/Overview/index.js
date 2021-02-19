ff6f4b1fab7ab1dce6c542d2e851295878520fdf
jasummercity-mac <jandavearce@jasummercity-macs-MacBook-Pro.local>
2021-02-19 09:34:00 +0800

    Refactor datepicker

--- assets/containers/Overview/index.js
+++ assets/containers/Overview/index.js
@@ -1,10 +1,12 @@
 import React, { useMemo } from 'react'
+import DatePicker from 'Components/DatePicker'
 
 export default function Overview() {
   return useMemo(() => {
     return (
       <>
         <h1>Overview Component</h1>
+        <DatePicker />
       </>
     )
   }, [])
--- assets/context/constant.js
+++ assets/context/constant.js
@@ -1,13 +1,3 @@
 export const SET_APP = 'SET_APP'
 export const SET_DATE_FILTER = 'SET_DATE_FILTER'
 export const SET_APP_ERROR = 'SET_APP_ERROR'
-
-export const TODAY = 'TODAY'
-export const LAST_7_DAYS = 'LAST_7_DAYS'
-export const LAST_14_DAYS = 'LAST_14_DAYS'
-export const LAST_30_DAYS = 'LAST_30_DAYS'
-export const CURRENT_MONTH = 'CURRENT_MONTH'
-export const PREVIOUS_MONTH = 'PREVIOUS_MONTH'
-export const PREVIOUS_QUARTER = 'PREVIOUS_QUARTER'
-export const ALL = 'ALL'
-export const CUSTOM = 'CUSTOM'
