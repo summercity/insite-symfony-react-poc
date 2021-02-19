776d0c8d46f6691113dd24f7967a521312323cf3
jasummercity-mac <jandavearce@jasummercity-macs-MacBook-Pro.local>
2021-02-19 12:05:57 +0800

    update main index

--- assets/containers/App/index.js
+++ assets/containers/App/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
-import { DateRangeContextProvider } from './../../context/DateRangeContext'
-// import styled from 'styled-components';
 import ContextStore from 'Context/ContextStore'
 import Dashboard from 'Containers/Dashboard'
 import Overview from 'Containers/Overview/Loadable'
@@ -10,31 +8,24 @@ import Login from 'Containers/Login'
 import NotFoundPage from 'Components/NotFoundPage'
 import ProtectedRoute from 'Components/ProtectedRoute'
 
-// const AppWrapper = styled.div`
-//     width: 100%;
-// `;
-
-// const test = () => (<ContentWrapper><Dashboard/></ContentWrapper>)
 export default function App() {
   return (
     <>
       <ContextStore>
-        <DateRangeContextProvider>
-          <Helmet titleTemplate="insiteLogic" defaultTitle="insiteLogic">
-            <meta name="description" content="insiteLogic" />
-          </Helmet>
-          <BrowserRouter>
-            <Switch>
-              <ProtectedRoute exact path="/" component={Dashboard} />
-              <ProtectedRoute exact path="/dashboard" component={Dashboard} />
-              <ProtectedRoute exact path="/overview" component={Overview} />
-              <Route exact path="/login" component={Login} />
-              <Route path="" component={NotFoundPage} />
-            </Switch>
-          </BrowserRouter>
-          {/* <Footer />
+        <Helmet titleTemplate="insiteLogic" defaultTitle="insiteLogic">
+          <meta name="description" content="insiteLogic" />
+        </Helmet>
+        <BrowserRouter>
+          <Switch>
+            <ProtectedRoute exact path="/" component={Dashboard} />
+            <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+            <ProtectedRoute exact path="/overview" component={Overview} />
+            <Route exact path="/login" component={Login} />
+            <Route path="" component={NotFoundPage} />
+          </Switch>
+        </BrowserRouter>
+        {/* <Footer />
           <GlobalStyle /> */}
-        </DateRangeContextProvider>
       </ContextStore>
     </>
   )
